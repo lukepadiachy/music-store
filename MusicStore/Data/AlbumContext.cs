b7ee1843369733c3add384e04a70335f56538243
Luke Padiachy <148848073+lukepadiachy@users.noreply.github.com>
2024-12-29 20:40:12 +0200

    cleaning up

--- MusicStore/Data/AlbumContext.cs
+++ MusicStore/Data/AlbumContext.cs
@@ -5,11 +5,9 @@ namespace MusicStore.Data
 {
     public class AlbumContext : DbContext
     {
-
         public AlbumContext(DbContextOptions<AlbumContext> options): base(options)
         {
         }
         public DbSet<Album> Albums { get; set; }
-
     }
 }
